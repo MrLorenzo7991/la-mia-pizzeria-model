254dccf2a9e02877257a32510a08c49927a3f411
Lorenzo <lorenzo.miccio97@gmail.com>
2022-05-10 12:20:56 +0200

    Aggiunto controller, modello e lista pizze

---
+++ la-mia-pizzeria/Models/Pizza.cs
@@ -0,0 +1,23 @@
+﻿namespace la_mia_pizzeria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredienti { get; set; } 
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(int id, string name, string desctiption, List<string> ingredienti)
+        {
+            this.Id=id;
+            this.Name=name;
+            this.Description=desctiption;
+            this.Ingredienti = ingredienti;
+        }
+    }
+}
---
+++ la-mia-pizzeria/Utils/PizzaData.cs
@@ -0,0 +1,16 @@
+﻿using la_mia_pizzeria.Models;
+
+namespace la_mia_pizzeria.Utils
+{
+    public static class PizzaData
+    {
+        private static List<Pizza> ListaPizze;
+        public static List<Pizza> GetPizze()
+        {
+            if (ListaPizze != null)
+            {
+                return ListaPizze;
+            }
+        }
+    }
+}
